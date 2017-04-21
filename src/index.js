1b64dd4bb7051ace1f7904e32bc2211033bc8800
Nitasha Kochar <nitashakochar@gmail.com>
2017-04-21 17:15:28 -0400

    added react

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 // change require to es6 import style
-import $ from 'jquery';
-import './style.scss';
+// import $ from 'jquery';
+// import './style.scss';
 
-let count = 0;
-function counter() {
-  count += 1;
-  $('#main').html(`You've been on this page for ${count} seconds.`);
-}
-
-setInterval(counter, 1000);
-
-
-// let counts = 0;
-// function update() {
-//   $('#main').html(counts);
-//   counts += 1;
+// let count = 0;
+// function counter() {
+//   count += 1;
+//   $('#main').html(`You've been on this page for ${count} seconds.`);
 // }
-// setInterval(update, 1000);
+
+// setInterval(counter, 1000);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
